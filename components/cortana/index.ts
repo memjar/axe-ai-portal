987e1049590d9b6b520dca689f12adffb74c589a
James Lewis <james@virul.co>
2026-02-15 21:07:40 -0500

    Remove dead code: old cortana consciousness page, ChatInterface, unused components

--- components/cortana/index.ts
+++
@@ -1,4 +0,0 @@
-export { ThoughtFeed } from './ThoughtFeed';
-export { StatsPanel } from './StatsPanel';
-export { StatusBar } from './StatusBar';
-export { ThoughtSkeleton } from './ThoughtSkeleton';
